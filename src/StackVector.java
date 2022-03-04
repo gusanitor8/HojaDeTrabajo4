1fb9cdeac1d227f5e1592f5a1b7dd0a8b00336d7
gusanitor8 <gusanitor8@gmail.com>
2022-03-04 16:23:41 -0600

    Create StackVector.java

---
+++ src/StackVector.java
@@ -0,0 +1,32 @@
+import java.util.Vector;
+
+public class StackVector<T> implements IStack<T> {
+	
+	private Vector<T> vector;
+
+	@Override
+	public void push(T value) {
+		vector.add(0, value);		
+	}
+
+	@Override
+	public T pull() {
+		return vector.remove(0);
+	}
+
+	@Override
+	public T peek() {
+		return vector.get(0);
+	}
+
+	@Override
+	public int count() {
+		return vector.size();
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return vector.isEmpty();
+	}
+
+}
