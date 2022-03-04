e6a00d3a2bc6312b8821ed4cfc1b494f99668baa
gusanitor8 <gusanitor8@gmail.com>
2022-03-04 17:29:54 -0600

    Cambios a Controlador y Vista

--- src/Controller.java
+++ src/Controller.java
@@ -1,9 +1,37 @@
 
 public class Controller {
-//-------------------------------------GUS-------------------------------------	
+//-------------------------------------GUS-------------------------------------
+	Vista vista = new Vista();
+	String infix; 
+	String postfix;
+	StackVector<Token> vector = new StackVector<Token>();
+	
 	public void run() {
+		switch(vista.mainMenu()) {
+		case 1:
+			break;
+		case 2:
+			break;
+		case 3:
+			break;
+		case 4:
+			break;
+		case 5:
+			System.exit(0);
+			break;
+		default:
+			break;
+		}
 		//TODO agregar metodos del controlador
 	}
+	
+	
+	public void toPostFixVector(String infix) {
+		for(int i = 0; i< infix.length(); i++) {
+			
+		}
+	}
+	
 	
 //-------------------------------------DIEGUIN-------------------------------------
 	
---
+++ src/Vista.java
@@ -0,0 +1,68 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+
+public class Vista {
+	Scanner input = new Scanner(System.in);
+	/**
+	 * Metodo de print
+	 * @param text texto que se despliega en la consola
+	 */
+	private void prnt(String text) {
+		System.out.println(text);
+	}
+	
+	/**
+	 * Devuelve un mensaje de error
+	 * @return cadena de texto que es el mensaje de error
+	 */
+	private String mismatchMessage() {
+		return "La opcion que ingreso es invalida. Intente de nuevo.";
+	}
+	
+	/**
+	 * Metodo de menu principal
+	 * @return regresa entero con la opcion que eligio el usuario
+	 */
+	public int mainMenu() {
+		int opcion = 0;
+		
+		try {
+			prnt("Bienvenido, ingrese la implementacion del conversor que desea: ");
+			prnt("1. Stack (Nodos)");
+			prnt("2. Lista Encadenada");
+			prnt("3. Lista Doblemente Encadenada");
+			prnt("4. Vector");
+			prnt("5. Salir");
+			
+			opcion = input.nextInt();
+			input.nextLine();
+			
+			if((opcion > 5) && (opcion < 1)) {
+				throw new Exception();
+			}
+			
+		}catch(InputMismatchException e) {
+			System.out.println(mismatchMessage());
+			input.nextLine();
+			opcion = mainMenu();
+			
+		}catch(Exception e) {
+			System.out.println(mismatchMessage());
+			opcion = mainMenu();
+		}
+		
+		
+		return opcion;
+	}
+	
+	/**
+	 * Devuelve la expresion en infix ingresada por el usuario
+	 * @return
+	 */
+	public String infixInput() {
+		String infix = input.nextLine();
+		return infix;
+	}
+	
+}
