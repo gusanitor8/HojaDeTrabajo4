acca5bce032d264ea8cc45fef34bda831965b95c
gusanitor8 <gusanitor8@gmail.com>
2022-03-04 16:26:46 -0600

    Update HojaDeTrabajo4.java

--- src/HojaDeTrabajo4.java
+++ src/HojaDeTrabajo4.java
@@ -1,4 +1,11 @@
 
 public class HojaDeTrabajo4 {
-
+	/**
+	 * Driver program
+	 * @param args cualquier cadena que se pase al momento de ejecucion
+	 */
+	public static void main(String[] args) {
+		Controller controller = new Controller();
+		controller.run();
+	}
 }
