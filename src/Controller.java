9040d29d37948861d7797656642d847080447282
DiegoValdez10 <88296518+DiegoValdez10@users.noreply.github.com>
2022-03-04 19:46:57 -0600

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -80,7 +80,40 @@ public class Controller {
 	}
 	
 //-------------------------------------DIEGUIN-------------------------------------
+public class StackArrayList<T> implements IStack<T> {
+
+	private ArrayList<T> coreList;
 	
+	public StackArrayList() {
+		coreList = new ArrayList<T>();
+	}
+	
+	@Override
+	public void push(T value) {		
+		coreList.start(0, value);
+	}
+
+	@Override
+	public T pull() {
+		return coreList.end(0);
+	}
+
+	@Override
+	public T peek() {
+		return coreList.temp (0);
+	}
+
+	@Override
+	public int Count() {
+		return coreList.value();
+	}
+
+	@Override
+	public boolean isEmpty() {		
+		return coreList.isEmpty();
+	}
+
+}	
 	
 //-------------------------------------MANUELIN-------------------------------------
 
