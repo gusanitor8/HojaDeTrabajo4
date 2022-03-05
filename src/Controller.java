b62aab3724b41a57a74a4d9d86e5e7a9db8d36b7
DiegoValdez10 <88296518+DiegoValdez10@users.noreply.github.com>
2022-03-04 20:57:32 -0600

    Update Controller.java

--- src/Controller.java
+++ src/Controller.java
@@ -81,7 +81,7 @@ public class Controller {
 	
 //-------------------------------------DIEGUIN-------------------------------------
 //Single List
-public class StackArrayList<T> implements IStack<T> {
+public void StackArrayList<T> implements IStack<T> {
 
 	private ArrayList<T> coreList;
 	
@@ -115,7 +115,41 @@ public class StackArrayList<T> implements IStack<T> {
 	}
 
 }	
+//DoubleList
+public void StackArrayListT<T> implements IStack<T> {
+
+	private ArrayListT<T> coreListT;
 	
+	public StackArrayListT() {
+		coreListT = new ArrayListT<T>();
+	}
+	
+	@Override
+	public void push(T value) {		
+		coreListT.startt(0, value);
+	}
+
+	@Override
+	public T pull() {
+		return coreListT.endt(0);
+	}
+
+	@Override
+	public T peek() {
+		return coreListT.tempt (0);
+	}
+
+	@Override
+	public int Count() {
+		return coreListT.valuet();
+	}
+
+	@Override
+	public boolean isEmpty() {		
+		return coreListT.isEmptyT();
+	}
+
+}
 //-------------------------------------MANUELIN-------------------------------------
 
 }
